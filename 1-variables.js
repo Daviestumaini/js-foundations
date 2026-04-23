e8453f6253b0e65a4e9e6d649f28b789db2c22c5
Davies Tumaini <daviestumaini@gmail.com>
2026-04-23 17:59:03 +0300

    variables

---
+++ 1-variables.js
@@ -0,0 +1,20 @@
+Variables are labeled storage for data. They are used to store and manipulate values in a program. In JavaScript, you can declare variables using the `var`, `let`, or `const` keywords. 
+There are three types of variables in JavaScript:
+Declarative variables: These tell the computer that a variable exists and can be incorporate a value. Example - 'let y = 5'
+Assignment variables: These add specific values to the variables that have been declared.
+Reference variables: These check if the variable has been declared and given a value.
+
+Here is an example of the following :
+
+//Declaration
+let currentscore = 0;
+const username = "Brian";
+//Reference
+console.log(username + " has a score of " + currentscore); // Output: Brian has a score of 0
+//Assignment
+currentscore = 15;
+console.log(username + " has a score of " + currentscore);
+ // Output: Brian has a score of 15
+ 
+ In the real world, variables are used everywhere in programming. They are ideal for storing a client's details and storing it efficiently on a database.
+ 
