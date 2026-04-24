3c6c64a097a12153e192e6e1dda28d125e749031
Davies Tumaini <daviestumaini@gmail.com>
2026-04-24 09:07:32 +0300

    data-types

---
+++ 2-data-types.js
@@ -0,0 +1,24 @@
+/* Data types are the fundamental blocks of programming. they show the interpreter how the programmer intends on using the data.
+In JavaScript there are 7 primitive data types: string, number, boolean, null, undefined, symbol, and bigint. There are also non-primitive data types such as objects and arrays.
+Here are some examples of the different data types:
+
+//String
+let name = "Brian"; 
+//Number
+let age = 30;
+//Boolean
+let isStudent = true;
+//Null
+let emptyValue = null;
+
+//Undefined
+let uninitializedVariable;
+//Symbol
+let uniqueId = Symbol("id");
+//BigInt
+let largeNumber = 9007199254740991n;
+
+Apart from the primitive data types there are also other data types such as objects and arrays which are non-primitive datat types that consist of multiple values.
+In the real world, data types are like containers in a kitchen. Just as you would use different containers for different types of food (e.g., a bowl for soup, a plate for salad), in programming, you use different data types to store and manipulate different kinds of information effectively. Understanding data types is crucial for writing efficient and error-free code.
+So, just as one wouldn't pour milk in a eggcarton, in programming, you wouldn't dare to store a string of text in a variable meant for numbers or a boolean value in a variablemeant for strings.
+This would make your code messy and very difficult to comprehend and maintain.*/
